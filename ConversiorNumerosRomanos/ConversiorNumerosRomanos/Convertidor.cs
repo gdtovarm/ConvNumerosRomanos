51ff57fb2871120f14100fd307bb1696e86aebe2
gdtovarm <gdtovarm@unal.edu.co>
2015-08-10 20:12:03 -0500

    Creada la clase convertidor, agregadas las definiciones de los numeros romanos.

---
+++ ConversiorNumerosRomanos/ConversiorNumerosRomanos/Convertidor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversiorNumerosRomanos
+{
+    class Convertidor
+    {
+        //Definicion de los números romanos.
+        string[] unidades, decenas, centenas, millares;
+
+        //Constructor, inicializa los arreglos de números romanos.
+        public Convertidor()
+        {
+            unidades = new string[10] { "","I","II","III","IV","V","VI","VII","VIII","XI" };
+            decenas = new string[10] { "","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+            centenas = new string[10] { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+            millares = new string[4] { "", "M", "MM", "MMM" };
+
+        }
+
+
+    }
+}
